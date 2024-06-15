d6ebd4fb2395f4bbdf0dc2a5cb2a9edab33ac330
Dmitry Pivovarov <pivovarov@mai.ru>
2024-06-15 15:50:40 +0300

    add unit-test: class slau

---
+++ src/tests/slau.cpp
@@ -0,0 +1,17 @@
+#define BOOST_TEST_MODULE geometry_toolbox
+#include <boost/test/included/unit_test.hpp>
+
+#include "slau.h"
+
+BOOST_AUTO_TEST_CASE(slau_test)
+{
+  const matrix_extended system = {{{{1.,2.,3.}},
+                                   {{4.,5.,6.}}}};
+  const column result_analyt = {{-1,2}};
+
+  const slau test_slau (system);
+  const column result_calced = test_slau.solve ();
+
+  BOOST_TEST(test_slau.is_consistent () == true);
+  BOOST_TEST(result_analyt == result_calced);
+}
