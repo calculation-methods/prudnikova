4249925e1936968a9994378967689c469e005d64
Dmitry Pivovarov <pivovarov@mai.ru>
2024-06-16 11:55:07 +0300

    add unit-test: liquid area calculation

--- src/tests/PLIC.cpp
+++ src/tests/PLIC.cpp
@@ -5,86 +5,160 @@
 #include "../structures.h"
 #include "../linear.h"
 
-BOOST_AUTO_TEST_SUITE(PLICTests)
+// BOOST_AUTO_TEST_SUITE(PLICTests)
 
-BOOST_AUTO_TEST_CASE(LineLineIntersectionTest)
+// BOOST_AUTO_TEST_CASE(LineLineIntersectionTest)
+// {
+//     line_segment l1 = {{1.0, 1.0}, 2.0};
+//     line_segment l2 = {{1.0, -1.0}, -1.0};
+//     point p;
+//     std::optional intersect = PLIC::line_line_intersection(l1, l2);
+//     BOOST_TEST(*intersect);
+//     BOOST_TEST(std::fabs((*intersect).x - 0.5) < 1e-10);
+//     BOOST_TEST(std::fabs((*intersect).y - 1.5) < 1e-10);
+
+// }
+
+// BOOST_AUTO_TEST_CASE(PointLocationTest)
+// {
+//     line_segment l1 = {{1.0, 1.0}, 2.0};
+//     point p1 = {3.0, 5.0};
+//     point p2 = {0.0, 0.0};
+//     point p3 = {0.0, 2.0};
+//     point p4 = {2.0, 2.0};
+//     point p5 = {2.0, 0.0};
+//     point p6 = {0.0, 1.0};
+//     BOOST_TEST(PLIC::point_to_line_relation(p1, l1));
+//     BOOST_TEST(!PLIC::point_to_line_relation(p2, l1));
+//     BOOST_TEST(PLIC::point_to_line_relation(p3, l1)); // boundary point
+//     BOOST_TEST(PLIC::point_to_line_relation(p4, l1));
+//     BOOST_TEST(PLIC::point_to_line_relation(p5, l1)); // boundary point
+//     BOOST_TEST(!PLIC::point_to_line_relation(p6, l1));
+// }
+
+// BOOST_AUTO_TEST_CASE(EmptyGrid)
+// {
+//     rectangular_grid emptyGrid;
+
+//     line_segment line = {{0, 0}, 1.0};
+
+//     polygon result = PLIC::collect_polygon_vertices(line, emptyGrid, 0, 0);
+
+//     BOOST_TEST(result.vertex_num == 0);
+//     BOOST_TEST(result.vertex.size() == 0);
+// }
+
+// BOOST_AUTO_TEST_CASE(LineIntersectsCell)
+// {
+//     rectangular_grid grid(1.0, 1.0, 3, 3);
+
+//     line_segment line = {{0, 1}, 1};
+
+//     polygon result = PLIC::collect_polygon_vertices(line, grid, 1, 1);
+
+//     point v1 = {1.0, 1.0};
+//     point v2 = {2.0, 1.0};
+//     point v3 = {2.0, 2.0};
+//     point v4 = {1.0, 2.0};
+
+//     BOOST_TEST(result.vertex_num == 4);
+//     BOOST_TEST(result.vertex.size() == 4);
+//     BOOST_TEST(result.vertex[0] == v1);
+//     BOOST_TEST(result.vertex[1] == v2);
+//     BOOST_TEST(result.vertex[2] == v3);
+//     BOOST_TEST(result.vertex[3] == v4);
+// }
+
+// BOOST_AUTO_TEST_CASE(form_polygon)
+// {
+//     line_segment line = {{1, 1}, 0.5};
+//     point v1 = {0.5, 0};
+//     point v2 = {1, 0};
+//     point v3 = {1, 1};
+//     point v4 = {0, 1};
+//     point v5 = {0, 0.5};
+
+//     rectangular_grid grid(1.0, 1.0, 3, 3);
+
+//     polygon result = PLIC::collect_polygon_vertices(line, grid, 0, 0);
+//     BOOST_TEST(result.vertex[0] == v1);
+//     BOOST_TEST(result.vertex[1] == v2);
+//     BOOST_TEST(result.vertex[2] == v3);
+//     BOOST_TEST(result.vertex[3] == v4);
+//     BOOST_TEST(result.vertex[4] == v5);
+// }
+// BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(interface_cutting)
+//  y
+//  ^  
+//  |    p1 - p2 - p3
+//  |    |    |    |
+//  |    p4 - p5 - p6
+//  |    |    |    |
+//  |    p7 - p8 - p9  
+//  |
+//  0-------------------> x
+//
+BOOST_AUTO_TEST_CASE(cut_triangle)
 {
-    line_segment l1 = {{1.0, 1.0}, 2.0};
-    line_segment l2 = {{1.0, -1.0}, -1.0};
-    point p;
-    std::optional intersect = PLIC::line_line_intersection(l1, l2);
-    BOOST_TEST(*intersect);
-    BOOST_TEST(std::fabs((*intersect).x - 0.5) < 1e-10);
-    BOOST_TEST(std::fabs((*intersect).y - 1.5) < 1e-10);
+	constexpr point p1 = {1, 3};
+	constexpr point p2 = {2, 3};
+	constexpr point p3 = {3, 3};
+	constexpr point p4 = {1, 2};
+	constexpr point p5 = {2, 2};
+	constexpr point p6 = {3, 2};
+	constexpr point p7 = {1, 1};
+	constexpr point p8 = {2, 1};
+	constexpr point p9 = {3, 1};
 
+  constexpr polygon cell {p1, p7, p9, p3};
+
+  constexpr line_equation interface (p2, p4);
+  constexpr double liquid_area = PLIC::cut_polygon_by_interface(interface, cell);
+  BOOST_TEST(liquid_area == 3.5);
+
+  constexpr line_equation interface_revert (p4, p2);
+  constexpr double gas_area = PLIC::cut_polygon_by_interface(interface, cell);
+  BOOST_TEST(gas_area == 0.5);
+
+  constexpr line_equation interface (p3, p7);
+  constexpr double liquid_area = PLIC::cut_polygon_by_interface(interface, cell);
+  BOOST_TEST(liquid_area == 3.5);
+
+  constexpr line_equation interface_revert (p7, p3);
+  const double gas_area = PLIC::cut_polygon_by_interface(interface, cell);
+  BOOST_TEST(gas_area == 0.5);
 }
 
-BOOST_AUTO_TEST_CASE(PointLocationTest)
+BOOST_AUTO_TEST_CASE(cut_trapezoid)
 {
-    line_segment l1 = {{1.0, 1.0}, 2.0};
-    point p1 = {3.0, 5.0};
-    point p2 = {0.0, 0.0};
-    point p3 = {0.0, 2.0};
-    point p4 = {2.0, 2.0};
-    point p5 = {2.0, 0.0};
-    point p6 = {0.0, 1.0};
-    BOOST_TEST(PLIC::point_to_line_relation(p1, l1));
-    BOOST_TEST(!PLIC::point_to_line_relation(p2, l1));
-    BOOST_TEST(PLIC::point_to_line_relation(p3, l1)); // boundary point
-    BOOST_TEST(PLIC::point_to_line_relation(p4, l1));
-    BOOST_TEST(PLIC::point_to_line_relation(p5, l1)); // boundary point
-    BOOST_TEST(!PLIC::point_to_line_relation(p6, l1));
-}
+	constexpr point p1 = {1, 3};
+	constexpr point p2 = {2, 3};
+	constexpr point p3 = {3, 3};
+	constexpr point p4 = {1, 2};
+	constexpr point p5 = {2, 2};
+	constexpr point p6 = {3, 2};
+	constexpr point p7 = {1, 1};
+	constexpr point p8 = {2, 1};
+	constexpr point p9 = {3, 1};
 
-BOOST_AUTO_TEST_CASE(EmptyGrid)
-{
-    rectangular_grid emptyGrid;
+  constexpr polygon cell {p1, p7, p9, p3};
 
-    line_segment line = {{0, 0}, 1.0};
+  constexpr line_equation interface (p4, p6);
+  constexpr double liquid_area = PLIC::cut_polygon_by_interface(interface, cell);
+  BOOST_TEST(liquid_area == 2.);
 
-    polygon result = PLIC::collect_polygon_vertices(line, emptyGrid, 0, 0);
+  constexpr line_equation interface_revert (p6, p4);
+  constexpr double gas_area = PLIC::cut_polygon_by_interface(interface, cell);
+  BOOST_TEST(gas_area == 2.);
 
-    BOOST_TEST(result.vertex_num == 0);
-    BOOST_TEST(result.vertex.size() == 0);
-}
+  constexpr line_equation interface (p4, p3);
+  constexpr double liquid_area = PLIC::cut_polygon_by_interface(interface, cell);
+  BOOST_TEST(liquid_area == 2.);
 
-BOOST_AUTO_TEST_CASE(LineIntersectsCell)
-{
-    rectangular_grid grid(1.0, 1.0, 3, 3);
-
-    line_segment line = {{0, 1}, 1};
-
-    polygon result = PLIC::collect_polygon_vertices(line, grid, 1, 1);
-
-    point v1 = {1.0, 1.0};
-    point v2 = {2.0, 1.0};
-    point v3 = {2.0, 2.0};
-    point v4 = {1.0, 2.0};
-
-    BOOST_TEST(result.vertex_num == 4);
-    BOOST_TEST(result.vertex.size() == 4);
-    BOOST_TEST(result.vertex[0] == v1);
-    BOOST_TEST(result.vertex[1] == v2);
-    BOOST_TEST(result.vertex[2] == v3);
-    BOOST_TEST(result.vertex[3] == v4);
-}
-
-BOOST_AUTO_TEST_CASE(form_polygon)
-{
-    line_segment line = {{1, 1}, 0.5};
-    point v1 = {0.5, 0};
-    point v2 = {1, 0};
-    point v3 = {1, 1};
-    point v4 = {0, 1};
-    point v5 = {0, 0.5};
-
-    rectangular_grid grid(1.0, 1.0, 3, 3);
-
-    polygon result = PLIC::collect_polygon_vertices(line, grid, 0, 0);
-    BOOST_TEST(result.vertex[0] == v1);
-    BOOST_TEST(result.vertex[1] == v2);
-    BOOST_TEST(result.vertex[2] == v3);
-    BOOST_TEST(result.vertex[3] == v4);
-    BOOST_TEST(result.vertex[4] == v5);
+  constexpr line_equation interface_revert (p3, p4);
+  const double gas_area = PLIC::cut_polygon_by_interface(interface, cell);
+  BOOST_TEST(gas_area == 2.);
 }
 BOOST_AUTO_TEST_SUITE_END()
